bf7e2378c8aafd9ed6b9d938125b642d81670373
Juan Camilo Bernal Gordillo <jcbernal-2024a@corhuila.edu.co>
2026-04-15 13:47:28 -0500

    Fix Frontend: Corrección de archivos docker, orquestamiento docker-compose 15/04/2026 13:47

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,8 +4,5 @@ set -e
 echo "⏳ Ejecutando migraciones de Prisma..."
 npx prisma migrate deploy
 
-echo "🚀 Iniciando backend NestJS..."
-exec node dist/main
-
 echo "✅ Schema sincronizado. Iniciando backend..."
 exec npm run start:dev
